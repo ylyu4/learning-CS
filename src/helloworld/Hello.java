bd0376c7148a6f402286165fe6c8fda429d48254
Apple <apple@AppledeMBP.verizon.net>
2019-11-18 03:48:00 -0500

    first repo ever

---
+++ src/helloworld/Hello.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+
+import utils.Solution;
+
+public class Hello{
+    public static void main(String[]args){
+       Solution solution = new Solution();
+        boolean result = Solution.isPrime(9);
+        System.out.println(result);
+    }
+}
---
+++ src/utils/Solution.java
@@ -0,0 +1,18 @@
+package utils;
+
+
+
+    public class Solution {
+        public boolean isPrime(int n) {
+            // Write your solution here
+            for(int i = 2; i*i <=n; i ++  ){
+                if (n%i  ==0){
+
+                }
+
+            }
+            return true;
+        }
+    }
+dasda
+dasda
