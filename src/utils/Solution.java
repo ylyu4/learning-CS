169c60ccd8e2112ddc741b37fb7e8432538c3cfe
ylyu4 <ylyu4@gwu.edu>
2019-11-18 03:53:17 -0500

    updated helloworld message

--- src/utils/Solution.java
+++ src/utils/Solution.java
@@ -7,12 +7,10 @@ package utils;
             // Write your solution here
             for(int i = 2; i*i <=n; i ++  ){
                 if (n%i  ==0){
-
+return false;
                 }
 
             }
             return true;
         }
     }
-dasda
-dasda
